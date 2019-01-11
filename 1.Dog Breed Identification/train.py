63bc1402e335668703d8db3752ce8f264a1b591c
ANG <ang_z@outlook.com>
2019-01-11 11:23:20 +0800

    针对SGD使用了lr迭代

--- 1.Dog Breed Identification/train.py
+++ 1.Dog Breed Identification/train.py
@@ -1,7 +1,8 @@
 from kaggleImageFolder import KaggleImageFolder
-from classifier import AngClassifier
+from classifier import AngClassifier, MixClassifier
 from torchvision.transforms import transforms
 import torch
+from torch.optim.lr_scheduler import StepLR
 
 from time import time
 import argparse
@@ -23,18 +24,19 @@ epochs = None
 save_per_iter = None
 
 
-def read_data(data_dir, mode, split_p=0.2):
+def read_data(data_dir, mode, labels_csv='labels.csv', split_p=0.2):
     if mode == 'train':
         data_transform = transforms.Compose([
+            transforms.Resize(224),
             transforms.RandomCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
+            # transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
     else:
         data_transform = transforms.Compose([
-            transforms.RandomCrop(224),
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
@@ -60,7 +62,7 @@ def accuracy(model, dataloader, n_data):
     return acc
 
 
-def train(model, train_dataloader, valid_dataloader, n_train, n_val, criterion, optimizer):
+def train(model, train_dataloader, valid_dataloader, n_train, n_val, criterion, optimizer, epochs=20, scheduler=None):
     train_loss_list = []
     train_acc_list = []
     valid_acc_list = []
@@ -68,6 +70,10 @@ def train(model, train_dataloader, valid_dataloader, n_train, n_val, criterion, 
     it = 1
 
     for epoch in range(epochs):
+        if scheduler is not None:
+            scheduler.step()
+
+        epoch_losses = []
         model.train()
         for i, (x, y) in enumerate(train_dataloader):
             if use_gpu:
@@ -76,53 +82,86 @@ def train(model, train_dataloader, valid_dataloader, n_train, n_val, criterion, 
             optimizer.zero_grad()
             z = model(x)
             loss = criterion(z, y)
-            train_loss_list.append(loss.data.item())
+            epoch_losses.append(loss.data.item())
             loss.backward()
             optimizer.step()
 
-            if i % 10 == 0:
-                print("Epoch: {}/{}...".format(epoch, epochs),
-                      "Loss: {:.4f}".format(loss))
-
-            # if it % save_per_iter == 0:
-            #     model.save_model(save_path+"/{}-{}-{}.pth".format(epoch, it, int(time())))
             it += 1
 
             if use_gpu:
                 torch.cuda.empty_cache()
 
+        mean_epoch_loss = sum(epoch_losses) / len(epoch_losses)
+        print("Epoch: {}/{}...".format(epoch + 1, epochs),
+              "Train Loss: {:.4f}".format(mean_epoch_loss))
+        train_loss_list.append(mean_epoch_loss)
+
         train_acc_list.append(accuracy(model, train_dataloader, n_train))
 
         if valid_dataloader is not None:
-            valid_acc_list.append(accuracy(model, valid_dataloader, n_val))
+            acc = accuracy(model, valid_dataloader, n_val)
+            if len(valid_acc_list) > 0 and acc > max(valid_acc_list):
+                model.save_model(save_path + '/{}-checkpoint-best.pth'.format(model.arch))
+                optimizer_params = {"optimizer_params": optimizer.state_dict()}
+                torch.save(optimizer_params, save_path + "/{}-{}-checkpoint-best.pth".format(
+                    model.arch, optimizer.__class__.__name__))
+            valid_acc_list.append(acc)
+
             print("Epoch: {}/{}... ".format(epoch + 1, epochs),
                   "Iter: {}".format(it),
-                  "valid accuarcy: {:.4f}".format(valid_acc_list[-1]))
-        else:
+                  "valid accuracy: {:.4f}".format(valid_acc_list[-1]))
             print("Epoch: {}/{}... ".format(epoch + 1, epochs),
                   "Iter: {}".format(it),
-                  "valid accuarcy: {:.4f}".format(train_acc_list[-1]))
+                  "train accuracy: {:.4f}".format(train_acc_list[-1]))
+        #
+        # if epoch % save_per_iter == 0:
+        #     model.save_model(save_path + '/{}-checkpoint-{}.pth'.format(model.arch, epoch))
+        #     optimizer_params = {"optimizer_params": optimizer.state_dict()}
+        #     torch.save(optimizer_params, save_path + "/{}-{}-checkpoint-{}.pth".format(
+        #         model.arch, optimizer.__class__.__name__, epoch))
 
     return train_loss_list, train_acc_list, valid_acc_list
 
 
-def main(model_file=None, param_file=None):
-    train_data = read_data(root + '/train', 'train', split_p=split_p)
-    valid_data = read_data(root + '/train', 'valid', split_p=split_p)
+def main(model_file=None, param_file=None, use_mix=False):
+    train_data = read_data(root + '/train', 'train', labels_csv, split_p=split_p)
+    valid_data = read_data(root + '/train', 'valid', labels_csv, split_p=split_p)
 
     if valid_data is not None:
         assert train_data.n_classes == valid_data.n_classes, "train_data.n_classes != valid_data.n_classes"
 
-    train_dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size)
-    valid_dataloader = torch.utils.data.DataLoader(dataset=valid_data, batch_size=batch_size)
+    train_dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
 
-    if model_file is None:
-        n_class = train_data.n_classes
-        model = AngClassifier(arch, hidden_units=hidden_units, n_class=n_class)
+    if len(valid_data) != 0:
+        valid_dataloader = torch.utils.data.DataLoader(dataset=valid_data, batch_size=batch_size)
     else:
-        model = AngClassifier(load_file=model_file)
+        valid_dataloader = None
 
-    optimizer = torch.optim.SGD(params=model.classifier.parameters(), momentum=0.9, lr=lr)
+    if use_mix:
+        if model_file is None:
+            model = MixClassifier(n_classes=train_data.n_classes)
+        else:
+            model = MixClassifier(load_file=model_file)
+    else:
+        if model_file is None:
+            n_class = train_data.n_classes
+            model = AngClassifier(arch, hidden_units=hidden_units, n_classes=n_class)
+        else:
+            model = AngClassifier(load_file=model_file)
+
+    # ignored_params = list(map(id, model.classifier.parameters()))
+    # base_params = filter(lambda p: id(p) not in ignored_params,
+    #                      model.parameters())
+    #
+    # optimizer = torch.optim.Adam([
+    #     {'params': base_params},
+    #     {'params': model.classifier.parameters(), 'lr': lr}
+    # ], lr=lr * 0.001)
+
+    # optimizer = torch.optim.SGD(params=model.classifier.parameters(), momentum=0.9, lr=lr, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, model.parameters()),
+                                 lr=lr, weight_decay=1e-5)
+    scheduler = StepLR(optimizer, step_size=40, gamma=0.1)
     criterion = torch.nn.CrossEntropyLoss()
 
     if use_gpu:
@@ -140,14 +179,18 @@ def main(model_file=None, param_file=None):
     n_train, n_val = len(train_data), len(valid_data)
 
     train_loss_list, train_acc_list, valid_acc_list = train(
-        model, train_dataloader, valid_dataloader, n_train, n_val, criterion, optimizer)
+        model, train_dataloader, valid_dataloader, n_train, n_val,
+        criterion, optimizer, epochs=epochs, scheduler=scheduler)
+
+    print('max valid accuracy: ', max(valid_acc_list))
 
     time_id = int(time())
 
     model.save_model(save_path+"/{}-checkpoint-{}.pth".format(model.arch, time_id))
 
     optimizer_params = {"optimizer_params": optimizer.state_dict()}
-    torch.save(optimizer_params, save_path+"/{}-optimizer-checkpoint-{}.pth".format(model.arch, time_id))
+    torch.save(optimizer_params, save_path+"/{}-{}-checkpoint-{}.pth".format(
+        model.arch, optimizer.__class__.__name__, time_id))
 
     return train_loss_list, train_acc_list, valid_acc_list
 
@@ -193,43 +236,46 @@ def test_train():
     # for test_arch in arches:
     #     arch = test_arch
     #     main()
-    model_file = '''F:/DATA/dog breed/save_model/inception_v3-checkpoint-1546407881.pth'''
-    param_file = '''F:/DATA/dog breed/save_model/inception_v3-optimizer-checkpoint-1546407881.pth'''
+    model_file = '''F:/DATA/dog breed/save_model/inception_v3-checkpoint-best.pth'''
+    param_file = '''F:/DATA/dog breed/save_model/inception_v3-optimizer-checkpoint-best.pth'''
     main(model_file, param_file)
 
 
 if __name__ == "__main__":
-    test_train()
+    # test_train()
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--data_directory', help='path for data', default='d:/DATA/dog breed', type=str)
-    # parser.add_argument('--save_dir', help='save model path', default='save_model', type=str)
-    # parser.add_argument('--labels_csv', help='labels.csv path', default='./labels.csv', type=str)
-    # parser.add_argument('--arch', help='chose pre-trained network(vgg13, vgg16)', default='densenet161', type=str)
-    # parser.add_argument('--split_p', help='split_p rate', default=0.2, type=float)
-    # parser.add_argument('--learning_rate', help='learning rate', default=0.01, type=float)
-    # parser.add_argument('--epochs', help='epochs', default=20, type=int)
-    # parser.add_argument('--save_per_iter', help='save model pre iterator', default=100, type=int)
-    # parser.add_argument('--batch_size', help='batch_size', default=128, type=int)
-    # parser.add_argument('--gpu', help='use gpu', default=torch.cuda.is_available(), type=bool)
-    # parser.add_argument('--hidden_units', help='hidden units for hidden layers', metavar='N', nargs='+',
-    #                     default=[2048, 512], type=int)
-    #
-    # args = parser.parse_args()
-    # root = args.data_directory
-    #
-    # save_path = os.path.join(root, args.save_dir)
-    # if not os.path.exists(save_path):
-    #     os.mkdir(save_path)
-    #
-    # labels_csv = args.labels_csv
-    # arch = args.arch
-    # split_p = args.split_p
-    # use_gpu = args.gpu
-    # lr = args.learning_rate
-    # epochs = args.epochs
-    # save_per_iter = args.save_per_iter
-    # batch_size = args.batch_size
-    # hidden_units = args.hidden_units
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_directory', help='path for data', default='D:/DATA/dog breed', type=str)
+    parser.add_argument('--save_dir', help='save model path', default='save_model', type=str)
+    parser.add_argument('--labels_csv', help='labels.csv path', default='./labels.csv', type=str)
+    parser.add_argument('--arch', help='chose pre-trained network(vgg13, vgg16)', default='resnet152', type=str)
+    parser.add_argument('--split_p', help='split_p rate', default=0.2, type=float)
+    parser.add_argument('--learning_rate', help='learning rate', default=1e-4, type=float)
+    parser.add_argument('--epochs', help='epochs', default=50, type=int)
+    parser.add_argument('--save_per_iter', help='save model pre iterator', default=20, type=int)
+    parser.add_argument('--batch_size', help='batch_size', default=8, type=int)
+    parser.add_argument('--gpu', help='use gpu', default=torch.cuda.is_available(), type=bool)
+    parser.add_argument('--hidden_units', help='hidadden units for hidden layers', metavar='N', nargs='+',
+                        default=[1024, 512, 265], type=int)
 
-    # main()
+    args = parser.parse_args()
+    root = args.data_directory
+
+    save_path = os.path.join(root, args.save_dir)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    labels_csv = args.labels_csv
+    arch = args.arch
+    split_p = args.split_p
+    use_gpu = args.gpu
+    lr = args.learning_rate
+    epochs = args.epochs
+    save_per_iter = args.save_per_iter
+    batch_size = args.batch_size
+    hidden_units = args.hidden_units
+
+    main(use_mix=True)
+    # model_file = '''D:/DATA/dog breed/save_model/MixClassifier-checkpoint-best.pth'''
+    # param_file = '''D:/DATA/dog breed/save_model/MixClassifier-Adam-checkpoint-best.pth'''
+    # main(model_file, None, use_mix=True)
